91975eb6c73f39781a149e8b9936bfd9bb169e88
Ansel <238927804+asong56@users.noreply.github.com>
2026-08-06 06:59:21 -0400

    Fix unsafe pointer casting in vp9.rs

--- vendor/vpx/src/lib/encoder/vp9.rs
+++ vendor/vpx/src/lib/encoder/vp9.rs
@@ -74,7 +74,7 @@ impl ::Interface for Interface {
 }
 impl InternalInterface for Interface {
     fn iface(&self) -> *mut ffi::vpx_codec_iface_t {
-        unsafe { &mut ffi::vpx_codec_vp9_cx_algo as *mut _ }
+        unsafe { &raw mut ffi::vpx_codec_vp9_cx_algo }
     }
 }
 
